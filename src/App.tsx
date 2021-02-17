aee3e0f749c269d73ec8e73a96ca651ebdd53492
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-17 16:28:02 +0100

    Add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { EventEmitter } from 'events'
+
+console.log(EventEmitter)
 
 interface AppProps {}
 
